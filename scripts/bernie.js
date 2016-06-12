10bcf8d51fc589c9df2b4ebaa14d3052d11d716a
= <rodolfobgibson@gmail.com>
2016-06-12 11:22:01 -0500

    Made image files on Choose your character page gifs. Also removed pngs.

--- scripts/bernie.js
+++ scripts/bernie.js
@@ -1,67 +1,42 @@
-@@ -0,0 +1,71 @@
-var player = new Player(getCookie("name"), getCookie("atkPow"), getCookie("def"), getCookie("evade"), getCookie("luck"), getCookie("health"));
-var enemy = new tempBoss('bernie', player );
+var bernie(){
+    function burn(){
+      var rand = Math.floor((Math.random()*10 ) +1);
+      if(rand==1){
+        document.getElementById('message').innerHTML = "Bernie's attack missed!"
+      }
+      else {
+        playerHP=playerHP-8
+      }
+    };
+    function twitterSupport(){
+      var rand = Math.floor((Math.random()*10 ) +1);
+      if(rand==1){
+        document.getElementById('message').innerHTML = "Bernie's attack missed!"
+      }
+      else {
+        playerHP=playerHP-8
+        document.getElementById('message').innerHTML = "Bernie's attack missed!"
+      }
+    };
+    function tuiton(){
+      var rand = Math.floor((Math.random()*10 ) +1);
+      if(rand==1){
+        document.getElementById('message').innerHTML = "Bernie's attack missed!"
+      }
+      else {
+        playerHP=playerHP-8
+        document.getElementById('message').innerHTML = "Bernie's attack missed!"
 
-//var oname = "Bernie";
-//var olevel = 5;
-//var oHP = 100;
-//oAttacks = [flameThrower, dragonClaw, ember, growl];
-
-var pname = getCookie("name");
-var plevel = 1;
-
-//var pHP = player.health;
-var playerTurn = true;
-
-$("#opHp").text(enemy.bossHealth);
-$("#myHp").text(Math.round(player.health));
-function playerAttack(num){
-+    var damage;
-+
-+    if(playerTurn){
-+        damage = player.attack(num);
-+        enemy.takeDam(damage);
-+        playerTurn = false;
-+
-+        $('#opHp').text(Math.round( enemy.bossHealth ) );
-+
-+    }
-+
-+}
-+function burn(){
-+    this.message="Bernie used Burn!";
-+    document.getElementById('message').innerHTML = Math.round( message );
-+}
-+function freeCollegeTution(){
-+    this.message="Bernie used Free College Tuition.";
-+    document.getElementById('message').innerHTML = Math.round( message );
-+
-+}
-+function internetSupport(){
-+    this.message="Bernie used internet support."
-+    document.getElementById('message').innerHTML = Math.round( message );
-+}
-+function fingerOfJustice(){
-+    this.message="Bernie used Finger of Justice!"
-+    document.getElementById('message').innerHTML = Math.round( message );
-+}
-+
-+function enemyAttack(){
-+    if(!playerTurn){
-+        var damage = enemy.attack();
-+        player.takeDamage(damage);
-+    }
-+    $('#myHp').text(Math.round(player.health));
-+}
-+
-+
-+
-+$( document ).ready(function() {
-+    $( ".oname" ).text("Bernie");
-+    $( ".olevel" ).text(1);
-+    $( ".oimage").attr('src',"./images/Bernie/sprite.gif");
-+
-+    $( ".pname" ).text(pname);
-+    $( ".plevel" ).text(plevel);
-+    $( ".pimage").attr('src',"./images/Player/guybattle.gif");
-+});
+      }
+    };
+    function fingerOfJustice(){
+      var rand = Math.floor((Math.random()*10 ) +1);
+      if(rand==1){
+        document.getElementById('message').innerHTML = "Bernie's attack missed!"
+      }
+      else {
+        playerHP=playerHP-8
+        document.getElementById('message').innerHTML = "Bernie's attack missed!"
+      }
+    };
+}
---
+++ scripts/player.js
@@ -0,0 +1,1 @@
+var playerHP=20;
